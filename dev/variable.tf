f43e5c52e4a105eab1744668668c2645e2811441
hamaa-affix <hamaagive1108@gmail.com>
2021-09-26 20:00:33 +0900

    create  private instace

--- dev/variable.tf
+++ dev/variable.tf
@@ -13,4 +13,16 @@ variable "azs" {
   default = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
 }
 
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "public_key_path" {
+  type = string
+}
 # /variable "rds_passwored" {}
